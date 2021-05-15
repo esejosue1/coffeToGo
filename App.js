e42d3ad61398a652fc899cff99560c854811917e
josue g <josuegallardolara@hotmail.com>
2021-05-14 18:43:58 -0700

    testing

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurant-screens";
-
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/insfrastructure/theme";
 // const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
+
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,25 @@
+//information of each restaurant in cards
+
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+import { RestaurantScreen } from "../screens/restaurant-screens";
+
+const Title = styled(Text)`
+  padding: 16px;
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "some restaurant",
@@ -15,27 +34,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover
-        key={name}
-        style={styles.imageCover}
-        source={{ uri: photos[0] }}
-      />
-      <Text style={styles.title}>{name}</Text>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "white",
-  },
-  imageCover: {
-    padding: 20,
-    backgroundColor: "white",
-  },
-  title: {
-    padding: 10,
-    fontSize: 30,
-  },
-});
