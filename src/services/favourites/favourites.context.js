3b72e9b5d2651a73e78d88d9301bc1e9e944ecfe
josue lara <josuegallardolara@hotmail.com>
2021-06-12 13:02:45 -0700

    Finished favorites component. Starting authentication process using firebase

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,11 +1,34 @@
 //favourites functionality
 
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  //save favourites restaurant in local phone storage
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (e) {
+      console.log("error saving ", e);
+    }
+  };
+
+  //load favourites restaurant in local phone storage
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (e) {
+      console.log("error loading ", e);
+    }
+  };
 
   //add to fav
   const add = (restaurant) => {
@@ -19,6 +42,16 @@ export const FavouritesContextProvider = ({ children }) => {
     );
     setFavourites(newFavourites);
   };
+
+  //load fav on every mount 
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  //listen to any change in fav, store fav
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
